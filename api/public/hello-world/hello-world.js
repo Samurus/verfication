e6baa8386efd8d11fb1fbf7a04044e61452fc1da
Samurus <achenbachj@web.de>
2024-05-07 12:52:02 +0200

    hello world

---
+++ api/public/hello-world/hello-world.js
@@ -0,0 +1,16 @@
+// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
+const handler = async (event) => {
+  try {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ "member": true }),
+      // // more keys you can return:
+      // headers: { "headerName": "headerValue", ... },
+      // isBase64Encoded: true,
+    }
+  } catch (error) {
+    return { statusCode: 500, body: error.toString() }
+  }
+}
+
+module.exports = { handler }

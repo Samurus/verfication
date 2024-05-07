f4216b900c4ad32ed6bac0e839f74b8b0792cdf2
Samurus <achenbachj@web.de>
2024-05-07 11:10:12 +0200

    cors

--- api/handler.js
+++ api/handler.js
@@ -1,3 +1,15 @@
+const cors = require('cors');
 module.exports = (req, res) => {
-    res.status(200).json({ member: true });
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*'); // Adjust this to be more restrictive if necessary
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+    if (req.method === 'OPTIONS') {
+        // Handle preflight request for CORS
+        return res.status(200).send();
+    }
+
+    // Your JSON response
+       res.status(200).json({ member: true });
 };

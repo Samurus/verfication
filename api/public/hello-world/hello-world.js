b8e4e9e2584e7bf01a8d8c5905bd76417ec88466
Samurus <achenbachj@web.de>
2024-05-07 12:57:40 +0200

    hello world

--- api/public/hello-world/hello-world.js
+++ api/public/hello-world/hello-world.js
@@ -1,8 +1,14 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = async (event) => {
+  const headers = {
+          'Access-Control-Allow-Origin': '*', // Adjust according to your needs
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+      };
   try {
     return {
       statusCode: 200,
+      headers,
       body: JSON.stringify({ "member": true }),
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },

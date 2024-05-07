92df07f9ca1b7f2faaec370fd6e9ecca2a96ef83
Samurus <achenbachj@web.de>
2024-05-07 10:33:17 +0200

    index

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
-module.exports = (req, res) => {
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = 3000;
+
+// Enable CORS for all requests
+app.use(cors());
+
+app.get('*', (req, res) => {
+    console.log(`registered player`);
     res.status(200).json({ member: true });
-};
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
